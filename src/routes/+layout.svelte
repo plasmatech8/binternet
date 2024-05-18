29ec6ae230e3c9f27658005f550cb67660650868
Mark Connelly <com0004@gmail.com>
2024-05-18 23:54:29 +1000

    Update layout and home

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -29,33 +29,19 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<a href="/" class="btn btn-sm hover:variant-soft-surface font-bold !gap-0 px-4">
+					<span class="text-3xl text-orange-400">₿</span>
+					<span class="text-xl uppercase !ml-0.5">internet</span>
+				</a>
 			</svelte:fragment>
+			<div>
+				<a class="btn btn-sm variant-ghost-surface" href="/documentation">Documentation</a>
+				<a class="btn btn-sm variant-ghost-surface" href="/create">Create</a>
+			</div>
 			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
+				<div>
+					<button class="btn variant-filled-primary">Connect Wallet</button>
+				</div>
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
