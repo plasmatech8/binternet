9795c2e2cb933ec6a2d27ae7f98d5b148123ca1c
Mark Connelly <com0004@gmail.com>
2024-05-26 22:34:48 +1000

    Format tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,28 @@
-import { join } from 'path'
-import type { Config } from 'tailwindcss'
+import { join } from 'path';
+import type { Config } from 'tailwindcss';
+// @ts-expect-error esModuleInterop not recognized
 import forms from '@tailwindcss/forms';
+// @ts-expect-error esModuleInterop not recognized
 import typography from '@tailwindcss/typography';
 import { skeleton } from '@skeletonlabs/tw-plugin';
-import { binternet } from './src/binternet'
+import { binternet } from './src/binternet';
 
 export default {
 	darkMode: 'class',
-	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+	],
 	theme: {
-		extend: {},
+		extend: {}
 	},
 	plugins: [
 		forms,
 		typography,
 		skeleton({
 			themes: {
-				custom: [
-					binternet,
-				],
-			},
-		}),
-	],
+				custom: [binternet]
+			}
+		})
+	]
 } satisfies Config;
