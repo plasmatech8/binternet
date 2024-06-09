449c9d6f5f2e3eca55ad1454ae6b878c827590b6
Mark Connelly <com0004@gmail.com>
2024-06-10 01:14:38 +1000

    Fix a11y warnings in svelte UX components spamming server terminal (https://github.com/techniq/svelte-ux/issues/186), format documents

--- svelte.config.js
+++ svelte.config.js
@@ -7,12 +7,16 @@ const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: [vitePreprocess()],
-	
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
+	},
+	onwarn(warning, defaultHandler) {
+		// Do not show 3rd party warnings
+		if (warning.filename.includes('node_modules')) return;
+		defaultHandler(warning);
 	}
 };
 export default config;
