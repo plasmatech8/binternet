0a4495cd2e0850aa1143022c46af01dded339776
Mark Connelly <com0004@gmail.com>
2024-06-26 23:03:45 +1000

    Remove unused API files

--- src/lib/backend-api/sources/__tests__/_cloudflare._test.ts
+++
@@ -1,6 +0,0 @@
-// import { describe, it } from 'vitest';
-// import { Cloudflare } from '../cloudflare';
-
-// const cloudflare = new Cloudflare();
-
-// describe('Cloudflare API', () => {});
--- src/lib/backend-api/sources/__tests__/_ordinalsBot._test.ts
+++
@@ -1,6 +0,0 @@
-// import { describe, it } from 'vitest';
-// import { OrdinalsBot } from '../ordinalsBot';
-
-// const ordinalsBot = new OrdinalsBot();
-
-// describe('OrdinalsBot API', () => {});
