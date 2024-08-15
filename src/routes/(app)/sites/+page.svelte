88dba7cc73c447045262a01423e4ce61a496afed
Mark Connelly <com0004@gmail.com>
2024-08-15 21:04:11 +1000

    Add message for listing sites not currently available for signet...

--- src/routes/(app)/sites/+page.svelte
+++ src/routes/(app)/sites/+page.svelte
@@ -5,6 +5,7 @@
 	import SiteCard from '../../../lib/components/cards/SiteCard.svelte';
 	import WalletButton from '$lib/components/wallet/WalletButton.svelte';
 	import { fetchSiteList } from '$lib/api';
+	import { PUBLIC_BITCOIN_NETWORK } from '$env/static/public';
 
 	let offset = 0;
 	let size = 0;
@@ -32,37 +33,41 @@
 	<h1 class="h1 mb-10">My Sites</h1>
 
 	{#if $wallet}
-		{#await fetchInscriptionList($wallet.ordinals, offset, limit)}
-			<div class="min-h-72 h-[50vh] grid place-items-center">
-				<ProgressRadial />
-			</div>
-		{:then sites}
-			<div class="flex flex-wrap gap-5 mb-10">
-				{#each sites as site}
-					<SiteCard
-						site={{
-							number: site.details.number,
-							router: site.router,
-							createdAt: site.details.createdAt
-						}}
-					></SiteCard>
-				{/each}
-			</div>
+		{#if PUBLIC_BITCOIN_NETWORK === 'mainnet'}
+			{#await fetchInscriptionList($wallet.ordinals, offset, limit)}
+				<div class="min-h-72 h-[50vh] grid place-items-center">
+					<ProgressRadial />
+				</div>
+			{:then sites}
+				<div class="flex flex-wrap gap-5 mb-10">
+					{#each sites as site}
+						<SiteCard
+							site={{
+								number: site.details.number,
+								router: site.router,
+								createdAt: site.details.createdAt
+							}}
+						></SiteCard>
+					{/each}
+				</div>
 
-			<div class="flex justify-center">
-				<Paginator
-					settings={{
-						page: offset / limit,
-						limit,
-						size,
-						amounts: []
-					}}
-					on:page={onPageChange}
-				></Paginator>
-			</div>
-		{:catch e}
-			{e.message}
-		{/await}
+				<div class="flex justify-center">
+					<Paginator
+						settings={{
+							page: offset / limit,
+							limit,
+							size,
+							amounts: []
+						}}
+						on:page={onPageChange}
+					></Paginator>
+				</div>
+			{:catch e}
+				{e.message}
+			{/await}
+		{:else}
+			<div>Listing sites is not currently available for {PUBLIC_BITCOIN_NETWORK}.</div>
+		{/if}
 	{:else}
 		<div class="flex flex-col justify-center items-center gap-14 h-[calc(70vh-152px)] min-h-72">
 			<div class="opacity-50">
