7cc98856237514ad728ee0368dcdb7f754bc9254
Mark Connelly <com0004@gmail.com>
2024-08-18 15:57:30 +1000

    Improve/fix migration

--- migrations/0001_inscriptions.sql
+++ migrations/0001_inscriptions.sql
@@ -1,4 +1,4 @@
--- Migration number: 0001 	 2024-06-16T13:22:14.096Z
+-- Migration number: 0001  2024-06-16T13:22:14.096Z
 /*
  * Inscriptions table
  */
@@ -7,5 +7,8 @@ create table inscriptions(
     id text not null,
     content_type text not null,
     created_at timestamptz default current_timestamp not null,
-    inscribed_at timestamptz not null,
+    inscribed_at timestamptz not null
 );
+
+-- Create an index for the `id` column
+create index idx_inscriptions_id on inscriptions(id);
