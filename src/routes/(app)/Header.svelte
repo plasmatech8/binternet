84c9a697b839edad3d78d0eb538ce80df847c8f4
Mark Connelly <com0004@gmail.com>
2024-09-25 21:48:29 +1000

    Add network switch button

--- src/routes/(app)/Header.svelte
+++ src/routes/(app)/Header.svelte
@@ -1,27 +1,65 @@
 <script lang="ts">
-	import { AppBar } from '@skeletonlabs/skeleton';
 	import { page } from '$app/stores';
-	import WalletButton from '$lib/components/wallet/WalletButton.svelte';
 	import { PUBLIC_BITCOIN_NETWORK } from '$env/static/public';
+	import WalletButton from '$lib/components/wallet/WalletButton.svelte';
+	import { AppBar, popup, type PopupSettings } from '@skeletonlabs/skeleton';
 	import pkg from '../../../package.json?raw';
 
 	const { version } = JSON.parse(pkg);
 	const isBeta = parseInt(version.split('.')[0]) < 1;
+
+	const networkSelectPopup: PopupSettings = {
+		event: 'click',
+		target: 'networkSelect',
+		placement: 'bottom'
+	};
+
+	const mainnetUrl = `https://binternet.org`;
+	const signetUrl = `https://signet.binternet.org`;
 </script>
+
+<div class="card w-48 shadow-xl rounded-2xl" data-popup={networkSelectPopup.target}>
+	<div class="list-nav z-30">
+		<ul>
+			<li>
+				<a href={mainnetUrl} class:variant-soft-primary={PUBLIC_BITCOIN_NETWORK === 'Mainnet'}>
+					Mainnet
+				</a>
+			</li>
+			<li>
+				<a href={signetUrl} class:variant-soft-primary={PUBLIC_BITCOIN_NETWORK === 'Signet'}>
+					Signet
+				</a>
+			</li>
+		</ul>
+	</div>
+</div>
 
 <AppBar gridColumns="sm:grid-cols-[auto_1fr_auto]">
 	<svelte:fragment slot="lead">
-		<a href="/" class="btn btn-sm hover:variant-soft-surface font-bold !gap-0 px-4">
-			<span class="text-3xl text-orange-400">₿</span>
-			<span class="text-xl uppercase !ml-0.5">internet</span>
-			{#if PUBLIC_BITCOIN_NETWORK !== 'mainnet'}
-				<span class="text-xs opacity-50">{PUBLIC_BITCOIN_NETWORK.toUpperCase()}</span>
-			{/if}
-			{#if isBeta}
-				<span class="text-xs opacity-50">BETA</span>
-			{/if}
-		</a>
+		<div class="flex gap-0 items-center">
+			<!-- Title -->
+			<a href="/" class="btn btn-sm hover:variant-soft-surface font-bold gap-0 px-4 relative">
+				<span class="text-3xl text-orange-400">₿</span>
+				<span class="text-xl uppercase !ml-0.5">internet</span>
+				<!-- Beta label -->
+				<div class="font-semibold leading-none absolute bottom-0 right-5">
+					{#if isBeta}
+						<div class="text-xs opacity-30">BETA</div>
+					{/if}
+				</div>
+			</a>
+			<!-- Network Switcher -->
+			<button
+				use:popup={networkSelectPopup}
+				class="text-xs btn btn-sm hover:variant-soft-surface font-bold gap-0 opacity-50"
+			>
+				<div>{PUBLIC_BITCOIN_NETWORK.toUpperCase()}</div>
+				<i class="fas fa-caret-down"></i>
+			</button>
+		</div>
 		<div class="block sm:hidden">
+			<!-- Wallet Button -->
 			<WalletButton morePopupOffset></WalletButton>
 		</div>
 	</svelte:fragment>
