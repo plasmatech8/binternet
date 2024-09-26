b8d1d4ce38e4d02c183440cfc3a113dede502f68
Mark Connelly <com0004@gmail.com>
2024-09-26 14:48:55 +1000

    Make dialog persistent with pulse animation, add refresh button

--- src/lib/components/modals/WaitingForSiteConfirmationModal.svelte
+++ src/lib/components/modals/WaitingForSiteConfirmationModal.svelte
@@ -22,57 +22,62 @@
 	 * Waiting for confirmation
 	 */
 	$: routerInscriptionId = txnId + 'i0';
-	let confirmedSiteDetails: (Site & { id: string }) | null = null;
-	$: siteLinkUrl = confirmedSiteDetails
-		? `${location.protocol}//${confirmedSiteDetails.number}.${PUBLIC_BITCOIN_NETWORK}.${location.host}`
+	let confirmedSite: WalletSitesResult | null = null;
+	$: siteLinkUrl = confirmedSite
+		? `${location.protocol}//${confirmedSite.details.number}.${PUBLIC_BITCOIN_NETWORK}.${location.host}`
 		: null;
 
 	blockStore.subscribe(() => {
-		setTimeout(async () => {
-			try {
-				const res = await axios.get<InscriptionDetails>(
-					`/api/inscription/${routerInscriptionId}/details`
-				);
-				if (res.status === 200 && !confirmedSiteDetails) {
-					console.log(`Router inscription ${routerInscriptionId} is now confirmed!`);
-					// Show success toast
-					toastStore.trigger({
-						message: 'Router inscription is now confirmed!',
-						background: 'variant-filled-success'
-					});
-					// Add confirmed site details
-					confirmedSiteDetails = {
-						number: res.data.number,
-						createdAt: new Date(res.data.inscribedAt).toISOString(),
-						router,
-						id: res.data.id
-					};
-					// Add to history
-					$siteHistoryStore = [...$siteHistoryStore, { details: confirmedSiteDetails, router }];
-					// Send request to save site to database
-					const baseRouteInscNumber = router.routes['/'];
-					let title = `Site ${confirmedSiteDetails.number}`;
-					if (baseRouteInscNumber) {
-						try {
-							const baseRouteContentRes = await axios.get(
-								`/api/inscription/${baseRouteInscNumber}/content`
-							);
-							const baseRouteContentData = baseRouteContentRes.data;
-							title = getTitleFromHTML(baseRouteContentData);
-						} catch (err) {
-							console.log('Failed to get site title:', err);
-							title = `Site ${confirmedSiteDetails.number}*`;
-						}
-					}
-					console.log('Site title is:', title);
-					axios.post(`/api/site`, {
-						number: res.data.number,
-						title: title
-					});
-				}
-			} catch (_) {}
-		}, 1000);
+		setTimeout(tryRefreshSiteConfirmation, 5000);
 	});
+
+	async function tryRefreshSiteConfirmation() {
+		try {
+			const res = await axios.get<InscriptionDetails>(
+				`/api/inscription/${routerInscriptionId}/details`
+			);
+			if (res.status === 200 && !confirmedSite) {
+				console.log(`Router inscription ${routerInscriptionId} is now confirmed!`);
+				// Show success toast
+				toastStore.trigger({
+					message: 'Router inscription is now confirmed!',
+					background: 'variant-filled-success'
+				});
+				// Add confirmed site details
+				confirmedSite = {
+					details: {
+						number: res.data.number,
+						inscribedAt: new Date(res.data.inscribedAt),
+						id: res.data.id,
+						contentType: res.data.contentType
+					},
+					router
+				};
+				// Add to history
+				$siteHistoryStore = [...$siteHistoryStore, confirmedSite];
+				// Send request to save site to database
+				const baseRouteInscNumber = router.routes['/'];
+				let title = `Site ${confirmedSite.details.number}`;
+				if (baseRouteInscNumber) {
+					try {
+						const baseRouteContentRes = await axios.get(
+							`/api/inscription/${baseRouteInscNumber}/content`
+						);
+						const baseRouteContentData = baseRouteContentRes.data;
+						title = getTitleFromHTML(baseRouteContentData);
+					} catch (err) {
+						console.log('Failed to get site title:', err);
+						title = `Site ${confirmedSite.details.number}*`;
+					}
+				}
+				console.log('Site title is:', title);
+				axios.post(`/api/site`, {
+					number: res.data.number,
+					title: title
+				});
+			}
+		} catch (_) {}
+	}
 
 	function truncateTxnId(txnId: string) {
 		return `${txnId.slice(0, 10)}...${txnId.slice(-10)}`;
@@ -82,14 +87,28 @@
 		$modalStore[0].response?.(resetForm);
 		modalStore.close();
 	}
+
+	let isPulsing = false;
 </script>
 
-<div class="card px-10 py-6 w-modal">
+<div class="w-full h-full fixed z-40" aria-hidden="true" on:click={() => (isPulsing = true)}></div>
+
+<div
+	class="card px-10 py-6 w-modal z-50 relative"
+	on:animationend={() => (isPulsing = false)}
+	class:pulse={isPulsing}
+>
 	<div class="flex flex-col gap-6">
 		<header class="text-3xl">Site Router Inscription</header>
-		{#if confirmedSiteDetails}
+		{#if confirmedSite}
 			<div>Your site is now inscribed and visible!</div>
-			<SiteRouterCodeBlock site={confirmedSiteDetails}></SiteRouterCodeBlock>
+			<SiteRouterCodeBlock
+				site={{
+					createdAt: confirmedSite.details.inscribedAt.toString(),
+					number: confirmedSite.details.number,
+					router: confirmedSite.router
+				}}
+			></SiteRouterCodeBlock>
 			<table>
 				<tr>
 					<td>Transaction ID:</td>
@@ -107,11 +126,11 @@
 					<td>Inscription number:</td>
 					<td>
 						<a
-							href={`${PUBLIC_INSCRIPTION_LINK_URL}/${confirmedSiteDetails.number}`}
+							href={`${PUBLIC_INSCRIPTION_LINK_URL}/${confirmedSite.details.number}`}
 							class="anchor no-underline hover:underline"
 							target="_blank"
 						>
-							{confirmedSiteDetails.number}
+							{confirmedSite.details.number}
 						</a>
 					</td>
 				</tr>
@@ -133,6 +152,14 @@
 				</button>
 			</div>
 		{:else}
+			<!-- Refresh button -->
+			<button
+				class="absolute top-4 right-4 btn-icon btn-icon-sm hover:variant-soft"
+				on:click={tryRefreshSiteConfirmation}
+			>
+				<i class="fas fa-refresh"></i>
+			</button>
+			<!-- Content -->
 			<div class="grid place-items-center gap-4 p-5">
 				<ProgressRadial />
 				<div>Waiting for router inscription confirmation...</div>
@@ -151,3 +178,20 @@
 		{/if}
 	</div>
 </div>
+
+<style>
+	@keyframes pulse {
+		0% {
+			transform: scale(1);
+		}
+		50% {
+			transform: scale(1.03);
+		}
+		100% {
+			transform: scale(1);
+		}
+	}
+	.pulse {
+		animation: pulse 0.2s ease-in-out;
+	}
+</style>
