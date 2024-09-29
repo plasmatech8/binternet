9fa6f944041c22c511ea937ee680dc0ab1262abe
Mark Connelly <com0004@gmail.com>
2024-09-29 21:41:44 +1000

    Add order status link

--- src/lib/components/modals/HistoryModal.svelte
+++ src/lib/components/modals/HistoryModal.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
-	import { PUBLIC_INSCRIPTION_LINK_URL, PUBLIC_TRANSACTION_LINK_URL } from '$env/static/public';
+	import {
+		PUBLIC_INSCRIPTION_LINK_URL,
+		PUBLIC_ORDER_STATUS_URL,
+		PUBLIC_TRANSACTION_LINK_URL
+	} from '$env/static/public';
 	import { orderHistoryStore } from '$lib/stores/history';
 	import { getModalStore } from '@skeletonlabs/skeleton';
 
@@ -23,65 +27,67 @@
 			<div class="flex gap-6 flex-wrap">
 				{#each $orderHistoryStore as orderStatus}
 					<div class="table-container outline outline-1 outline-primary-500">
-						<table class="table">
-							<tbody>
-								<tr>
-									<td><strong>Order ID:</strong></td>
-									<td>{orderStatus.id}</td>
-								</tr>
-								<tr>
-									<td><strong>Created:</strong></td>
-									<td>{new Date(orderStatus.createdAt).toLocaleString()}</td>
-								</tr>
-								<tr>
-									<td><strong>Status:</strong></td>
-									<td>{orderStatus.state}</td>
-								</tr>
-								<tr>
-									<td><strong>Files inscribed:</strong></td>
-									<td>
-										<div>
-											(Count: {orderStatus.files.length})
-										</div>
-										<ul class="list-disc ml-5">
-											{#each orderStatus.files as file}
-												<li>
-													<div class="flex gap-2">
-														<span>
-															{file.name}
-														</span>
-														{#if file.tx?.reveal}
-															<a
-																class="anchor"
-																href={`${PUBLIC_TRANSACTION_LINK_URL}/${file.tx.reveal}`}
-																target="_blank"
-															>
-																txn
-															</a>
-															<a
-																class="anchor"
-																href={`${PUBLIC_INSCRIPTION_LINK_URL}/${file.inscriptionId}`}
-																target="_blank"
-															>
-																link
-															</a>
-														{/if}
-													</div>
-												</li>
-											{/each}
-										</ul>
-									</td>
-								</tr>
-								<tr>
-									<td><strong>Receive address:</strong></td>
-									<td>{orderStatus.receiveAddress}</td>
-								</tr>
-								<tr>
-									<td><strong>Payment:</strong></td>
-									<td>{!!orderStatus.paid}</td>
-								</tr>
-							</tbody>
-						</table>
+						<a href={PUBLIC_ORDER_STATUS_URL + orderStatus.id} target="_blank">
+							<table class="table">
+								<tbody>
+									<tr>
+										<td><strong>Order ID:</strong></td>
+										<td>{orderStatus.id}</td>
+									</tr>
+									<tr>
+										<td><strong>Created:</strong></td>
+										<td>{new Date(orderStatus.createdAt).toLocaleString()}</td>
+									</tr>
+									<tr>
+										<td><strong>Status:</strong></td>
+										<td>{orderStatus.state}</td>
+									</tr>
+									<tr>
+										<td><strong>Files inscribed:</strong></td>
+										<td>
+											<div>
+												(Count: {orderStatus.files.length})
+											</div>
+											<ul class="list-disc ml-5">
+												{#each orderStatus.files as file}
+													<li>
+														<div class="flex gap-2">
+															<span>
+																{file.name}
+															</span>
+															{#if file.tx?.reveal}
+																<a
+																	class="anchor"
+																	href={`${PUBLIC_TRANSACTION_LINK_URL}/${file.tx.reveal}`}
+																	target="_blank"
+																>
+																	txn
+																</a>
+																<a
+																	class="anchor"
+																	href={`${PUBLIC_INSCRIPTION_LINK_URL}/${file.inscriptionId}`}
+																	target="_blank"
+																>
+																	link
+																</a>
+															{/if}
+														</div>
+													</li>
+												{/each}
+											</ul>
+										</td>
+									</tr>
+									<tr>
+										<td><strong>Receive address:</strong></td>
+										<td>{orderStatus.receiveAddress}</td>
+									</tr>
+									<tr>
+										<td><strong>Payment:</strong></td>
+										<td>{!!orderStatus.paid}</td>
+									</tr>
+								</tbody>
+							</table>
+						</a>
 					</div>
 				{:else}
 					<div class="grid place-items-center w-full h-40 opacity-50 font-semibold text-xl">
